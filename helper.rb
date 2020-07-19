212b4228a4acf9cde219fc0a4f2478cabd527b15
daniilty <adef96@mail.ru>
2020-07-19 21:29:55 +0300

    INC

---
+++ helper.rb
@@ -0,0 +1,33 @@
+require 'httparty'
+require 'nokogiri'
+
+class Info
+
+  attr_accessor :page
+  
+  def initialize()
+      doc = HTTParty.get("https://www.gismeteo.ru/")
+      @page = Nokogiri::HTML(doc)
+  end
+  def prin()
+    string = ""
+    string1 = ""
+    @page.xpath('//span[@class = "value unit unit_temperature_c"]').each do |el|
+      string += el.text
+    end
+    @page.xpath('//div[@class = "description gray"]').each do |el|
+      string1 += el.text
+    end
+    arr = string.split(' ')
+    string = arr[0] + ' ' + string1.split(' ')[0]
+    File.open('weather.txt', 'w') {  |file| 
+        file.write(string)
+    }
+  end
+
+end
+# print("Enter the tracking number: ")
+# inf = gets
+# inst = Info.new(inf.slice(..-2))
+inst = Info.new()
+inst.prin
